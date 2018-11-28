9af35ccb2a009e7e768aef39c5b1fd33a5df99b6
chandanNapster <chandan202.alive@gmail.com>
2018-11-28 19:15:18 +1300

    Initial commit

---
+++ src/Neo_Java_MVC/Edge.java
@@ -0,0 +1,15 @@
+package Neo_Java_MVC;
+
+public class Edge {
+
+    private int value;
+
+    public Edge(int value){
+        this.value = value;
+    }
+
+    public String toString(){
+        return "Weight" + "->" + value;
+    }
+
+}
